d2e069cf6b12f99ea9033f71139dd35a1e5bbea5
tsztin0217 <tsztin0217@gmail.com>
2025-02-23 22:40:16 -0500

    Updated init, runGame, render, randomNum0to3, and updateStes function

--- js/app.js
+++ js/app.js
@@ -17,12 +17,34 @@ const feedBtnEl = document.getElementById('feed');
 const sleepBtnEl = document.getElementById('sleep');
 const gameMessageEl = document.getElementById('message');
 const resetBtnEl = document.getElementById('restart');
-
-console.dir(resetBtnEl)
 /*-------------------------------- Functions --------------------------------*/
+const init = () => {
+    gameOver = false;
+    timer = setInterval(runGame(), 2000);
+}
 
+const runGame = () => {
+    updateStates();
+    render();
+}
 
+const render = () => {
+    boredomStatEl.textContent = state.boredom;
+    hungerStatEl.textContent = state.hunger;
+    sleepinessStatEl.textContent = state.sleepiness;
+};
 
+const randomNum0to3 = () => {
+    return Math.floor(Math.random() * 4);
+}
+
+const updateStates = () => {
+    state.boredom += randomNum0to3;
+    state.hunger += randomNum0to3;
+    state.sleepiness += randomNum0to3;
+}
+
+init();
 /*----------------------------- Event Listeners -----------------------------*/
 
 
