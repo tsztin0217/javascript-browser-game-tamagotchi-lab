cd02763df6a522d1b2beb858081b636bf6be8c1d
tsztin0217 <tsztin0217@gmail.com>
2025-02-23 22:09:16 -0500

    created constant, variables, and cached element references

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
-
+const state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0
+};
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let timer;
+let gameOver;
 
 /*------------------------ Cached Element References ------------------------*/
+const boredomStatEl = document.getElementById('boredom-stat');
+const hungerStatEl = document.getElementById('hunger-stat');
+const sleepinessStatEl = document.getElementById('sleepiness-stat');
+const playBtnEl = document.getElementById('play');
+const feedBtnEl = document.getElementById('feed');
+const sleepBtnEl = document.getElementById('sleep');
+const gameMessageEl = document.getElementById('message');
+const resetBtnEl = document.getElementById('restart');
 
-
-
+console.dir(resetBtnEl)
 /*-------------------------------- Functions --------------------------------*/
 
 
